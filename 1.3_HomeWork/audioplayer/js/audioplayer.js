9dabdebefa4721525fc586dae6a05e69462fa5b0
Noxser <noxser@gmail.com>
2018-06-29 11:55:07 +0500

    переработал кнопку Play Pause

--- 1.3_HomeWork/audioplayer/js/audioplayer.js
+++ 1.3_HomeWork/audioplayer/js/audioplayer.js
@@ -2,8 +2,9 @@
 
 const track = ['LA Chill Tour.mp3', 'LA Fusion Jam.mp3', 'This is it band.mp3'];
 
-const playButton = document.getElementsByClassName('fa-play')[0];
-const pauseButton = document.getElementsByClassName('fa-pause')[0];
+// const playButton = document.getElementsByClassName('fa-play')[0];
+// const pauseButton = document.getElementsByClassName('fa-pause')[0];
+const playstate = document.getElementsByClassName('playstate')[0];
 const stopButton = document.getElementsByClassName('stop')[0];
 const buttonNextTrack = document.getElementsByClassName('next')[0];
 const buttonPrevTrack = document.getElementsByClassName('back')[0];
@@ -11,22 +12,22 @@ const player = document.getElementsByTagName('audio')[0]
 const mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 const title = document.getElementsByClassName('title')[0];
 
-function buttonClickPlay() {
-    mediaplayer.classList.add('play');
-    player.play();
-    // проверяем играет ли музыка если нет то отключаем анимамцию
-    function stopThenEnd() {
-        if (player.ended) {
-            buttonClickStop(); // отключаем анимацию
-            clearInterval(timer); // убиваем таймер
+function buttonClickPlayPause() {
+    if (mediaplayer.classList.contains('play')) {
+        mediaplayer.classList.remove('play');
+        player.pause();
+    } else {
+        mediaplayer.classList.add('play');
+        player.play();
+        // проверяем играет ли музыка если нет то отключаем анимамцию
+        function stopThenEnd() {
+            if (player.ended) {
+                buttonClickStop(); // отключаем анимацию
+                clearInterval(timer); // убиваем таймер
+            }
         }
+        var timer = setInterval(stopThenEnd, 1000); // по таймеру проверяем каждую секунду
     }
-    var timer = setInterval(stopThenEnd, 1000); // по таймеру проверяем каждую секунду
-}
-
-function buttonClickPause() {
-    mediaplayer.classList.remove('play');
-    player.pause();
 }
 
 function buttonClickStop() {
@@ -44,7 +45,7 @@ function buttonClickNext() {
     }
     player.src = "mp3/" + track[step];
     if (mediaplayer.classList.contains('play')) {
-        buttonClickPlay()
+        player.play();
     }
     title.title = track[step].replace('.mp3','')
 }
@@ -54,13 +55,12 @@ function buttonClickPrev() {
     if (step < 0) { step = track.length + step}
     player.src = "mp3/" + track[step];
     if (mediaplayer.classList.contains('play')) {
-        buttonClickPlay()
+        player.play();
     }
     title.title = track[step].replace('.mp3','')
 }
 
+playstate.onclick = buttonClickPlayPause;
 buttonNextTrack.onclick = buttonClickNext;
 buttonPrevTrack.onclick = buttonClickPrev;
-playButton.onclick = buttonClickPlay;
-pauseButton.onclick = buttonClickPause;
 stopButton.onclick = buttonClickStop;
