b4e5faf0fc4feabebeefb623cd5ad2c9eb9210a3
Noxser <noxser@gmail.com>
2018-07-05 18:43:15 +0500

    Изменил формирование HTML кода

--- 2.2_HomeWork/contact-list/js/contact-list.js
+++ 2.2_HomeWork/contact-list/js/contact-list.js
@@ -5,7 +5,7 @@ var contactList = document.getElementsByClassName('contacts-list')[0];
 var contacts = JSON.parse(loadContacts());
 
 function createHTML(data) {
-    return '<li data-email=\'' + data.email + '\'data-phone=\'' + data.phone + '\'><strong>' + data.name + '</strong></li>';
+    return `<li data-email=${data.email} data-phone="${data.phone}"><strong>${data.name}</strong></li>`;
 }
 
 function start() {
