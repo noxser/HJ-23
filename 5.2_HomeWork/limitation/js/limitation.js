adcbf0cdeeba8bd821322f792e9a8854efc9a815
Noxser <noxser@gmail.com>
2018-08-09 13:32:09 +0500

    поправил зверька

--- 5.2_HomeWork/limitation/js/limitation.js
+++ 5.2_HomeWork/limitation/js/limitation.js
@@ -3,6 +3,7 @@
 const textarea = document.querySelector('.textarea');
 const block = document.querySelector('.block');
 const message = document.querySelector('.message');
+let mouseOut = false;
 
 // оптимизируем ввод
 function debounce(callback, delay) {
@@ -16,34 +17,69 @@ function debounce(callback, delay) {
     };
 }
 
-// при клике по полю ожидаем ввод если нет то сообщение 
-textarea.addEventListener('focus',debounce(() => {
-    block.classList.remove('active')
-    message.classList.add('view')
-    },2000)
+textarea.addEventListener('focus',() => {
+    block.classList.add('active')
+    }
+)
+
+textarea.addEventListener('click',() => {
+    block.classList.add('active')
+    message.classList.remove('view')
+    }
 )
 
 // сбрасываем состояние при начале ввода в поле
 textarea.addEventListener('input',() => {
     block.classList.add('active')
     message.classList.remove('view')
+    mouseOut = false;
 })
 
 // при остановке ввода более 2 сек сообщение
 textarea.addEventListener('keydown', debounce(() => {
-    block.classList.remove('active')
-    message.classList.add('view')
-    }, 2000)
+    if (!mouseOut) {
+      block.classList.remove('active')
+      message.classList.add('view')
+      }}, 2000)
 )
 
 // реагируем по движению мыши
-textarea.addEventListener('mouseover',() => {
-    block.classList.add('active')
-    message.classList.remove('view')
-})
-
 textarea.addEventListener('mouseout',() => {
     block.classList.remove('active')
     message.classList.remove('view')
+    mouseOut = true;
 })
+
+
+// **********************************************
+// // при клике по полю ожидаем ввод если нет то сообщение 
+// textarea.addEventListener('focus',debounce(() => {
+//     block.classList.remove('active')
+//     message.classList.add('view')
+//     },2000)
+// )
+
+// // сбрасываем состояние при начале ввода в поле
+// textarea.addEventListener('input',() => {
+//     block.classList.add('active')
+//     message.classList.remove('view')
+// })
+
+// // при остановке ввода более 2 сек сообщение
+// textarea.addEventListener('keydown', debounce(() => {
+//     block.classList.remove('active')
+//     message.classList.add('view')
+//     }, 2000)
+// )
+
+// // реагируем по движению мыши
+// textarea.addEventListener('mouseover',() => {
+//     block.classList.add('active')
+//     message.classList.remove('view')
+// })
+
+// textarea.addEventListener('mouseout',() => {
+//     block.classList.remove('active')
+//     message.classList.remove('view')
+// })
 
